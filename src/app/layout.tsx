51d4ebcd8b6e13265223f5d9097b1c55e95efa89
yangxin <yangxin.feiu@bytedance.com>
2026-05-08 19:18:30 +0800

    修改node版本为 24
    Change-Id: Iba437053a5f9d09c927c2cdca0deeb461213b563

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,5 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
-
-// create Google Fonts for the project
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'AI Chatbot',
@@ -14,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
